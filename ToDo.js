92bf123d05952592f8df0b57e4c69c1a369e665d
Inmobi Korea <taki1984@naver.com>
2019-08-26 10:49:54 +0900

    UI part.3 End.

--- ToDo.js
+++ ToDo.js
@@ -1,42 +1,62 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Dimensions } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Dimensions
+} from "react-native";
 
-const { height, width } = Dimensions.get("window")
+const { height, width } = Dimensions.get("window");
 
 export default function ToDo() {
+  const [isEditing, setIsEditing] = useState(false);
+  const [isCompleted, setIsCompleted] = useState(false);
 
-    const [isEditing, setIsEditing] = useState(false);
-    const [isComplete, setIsComplete] = useState(false);
-
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity onPress={() => { setIsComplete(!isComplete) }}>
-                <View style={styles.circle}></View>
-            </TouchableOpacity>
-            <Text style={styles.text}>hello</Text>
-        </View >
-    );
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          setIsCompleted(!isCompleted);
+        }}
+      >
+        <View
+          style={[
+            styles.circle,
+            isCompleted ? styles.completedCircle : styles.uncompletedCircle
+          ]}
+        />
+      </TouchableOpacity>
+      <Text style={styles.text}>hello</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: width - 50,
-        borderBottomColor: "#bbb",
-        borderBottomWidth: StyleSheet.hairlineWidth,
-        flexDirection: "row",
-        alignItems: "center"
-    },
-    circle: {
-        width: 30,
-        height: 30,
-        borderRadius: 15,
-        borderColor: "red",
-        borderWidth: 3,
-        marginRight: 20
-    },
-    text: {
-        fontWeight: "600",
-        fontSize: 20,
-        marginVertical: 20
-    }
+  container: {
+    width: width - 50,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  circle: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderColor: "red",
+    borderWidth: 3,
+    marginRight: 20
+  },
+  completedCircle: {
+    borderColor: "#bbb"
+  },
+  uncompletedCircle: {
+    borderColor: "#f23657"
+  },
+  text: {
+    fontWeight: "600",
+    fontSize: 20,
+    marginVertical: 20
+  }
 });
